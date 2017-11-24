422bc09207ffd8d5559880acee7f27341ec0ec34
maless79 <maless79@gmail.com>
2017-11-24 22:11:24 +0100

    testUpload3

--- wywolanieSilnia.py
+++ wywolanieSilnia.py
@@ -1,9 +1,19 @@
 from nowy import silnia
 import wypelnianieListy
+import random
 import rekurencja
+i=0
+while i<20:
+    #z = random.randint(1, 10)
+    z = random.randint(5, 25)
+    if z%2==0:
+        print(z)
+    else:
+        continue
+    i+=1
 
-a=rekurencja.rekurencja_def(5)
-
+#a=rekurencja.rekurencja_def(5)
+#print ("Random rowna sie= ", z)
 #wielkosc_listy2 = input("wprowadz wielkosc silni: ")
 #wydruk=silnia(int(wielkosc_listy2))
 #wielkosc_listy = input("wprowadz wielkosc listy: ")
