4c68d8b898fe305626a5f0af64bc09ed7a91672a
maless79 <maless79@gmail.com>
2017-11-24 22:31:00 +0100

    testUpload3

---
+++ Postacie.py
@@ -0,0 +1,10 @@
+class Postacie():
+    def __init__(self):
+        s=1
+        w=2
+        print ("Skonstruowalem Potacie__init - SELF")
+    def zwroc():
+        x =s+w
+        return (x)
+    def __del__(self):
+        print ("Zniszylem Potacie__init - SELF")
