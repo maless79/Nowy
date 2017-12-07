84bcdbe1192b7f4d530fc12fdca118f9fa4727cb
maless79 <maless79@gmail.com>
2017-12-07 20:51:45 +0100

    Airpunk

--- Airpunk/Airpunk_Cechy.py
+++ Airpunk/Airpunk_Cechy.py
@@ -4,10 +4,10 @@ def cechy_postaci(rasa=0):
 
     rcpkzmas = {
         "refleks":random.randint(1, 10),
-        "cialo":random.randint(1, 10),
+        "ciało":random.randint(1, 10),
         "psychika":random.randint(1, 10),
         "koordynacja":random.randint(1, 10),
-        "zmysly":random.randint(1, 10),
+        "zmysły":random.randint(1, 10),
         "mechanika":random.randint(1, 10),
         "akcja":random.randint(1, 10),
         "sterowanie":random.randint(1, 10),
@@ -18,14 +18,14 @@ def cechy_postaci(rasa=0):
     elif rasa==1:
         rcpkzmas["refleks"] = Walidatory.walidacja([rasa,1, random.randint(1, 12)])
     elif rasa==2:
-        rcpkzmas["cialo"] = Walidatory.walidacja([rasa,1, random.randint(1, 12)])
+        rcpkzmas["ciało"] = Walidatory.walidacja([rasa,1, random.randint(1, 12)])
     else:
         rcpkzmas={
             "refleks":0,
-            "cialo":0,
+            "ciało":0,
             "psychika":0,
             "koordynacja":0,
-            "zmysly":0,
+            "zmysły":0,
             "mechanika":0,
             "akcja":0,
             "sterowanie":0,
